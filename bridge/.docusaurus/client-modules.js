32d1b706e83ea4e314ecd98f8ef48a7b2d542321
Zach Kelling <z@zeekay.io>
2024-05-19 14:11:54 +0800

    Remove generated files

--- bridge/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/z/work/lux/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/z/work/lux/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/z/work/lux/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/z/work/lux/docs/src/css/custom.css"),
-];
